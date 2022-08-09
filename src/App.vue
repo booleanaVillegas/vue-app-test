943ef968974778b7bd5949746e9e9e57086bbb17
Juliana Villegas <jumavipe@hotmail.com>
2022-08-09 07:29:29 -0500

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<script>
+import DisplayConcept from './components/DisplayConcept.vue'
+
+export default {
+  components: {
+    DisplayConcept
+  }
+}
+</script>
+
+<template>
+  <header><h1>Vue Conceptos Basicos</h1></header>
+
+  <main>
+    <DisplayConcept /> 
+  </main>
+
+  <footer>Clase de programación web avanzada</footer>
+</template>
+
+<style scoped>
+header {
+  background-color: cadetblue;
+  color: white;
+  width: 100%;
+}
+
+h1 {
+  margin: 0 auto;
+  text-align: center;
+}
+
+main {
+  display: flex;
+  flex-direction: column;
+  justify-items: center;
+}
+</style>
---
+++ src/components/DisplayConcept.vue
@@ -0,0 +1,27 @@
+<script>
+export default {
+  data() {
+    return {
+      isSelected : true
+    }
+  }
+}
+</script>
+
+<template>
+ <section>
+  <h2>Data, Methods, Computed</h2>
+  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam dolorum itaque aperiam quos harum enim veritatis, possimus ipsa distinctio repellat, tenetur esse earum voluptatibus consectetur repellendus nam non consequuntur pariatur!</p>
+  <code>{
+    data: isSelected}</code>
+ </section>
+</template>
+
+<style scoped>
+section {
+  margin: 20px;
+  padding: 20px;
+  background-color: #EEE;
+  width: 1000px;
+}
+</style>
