2a85e5a9bc07f630a757688185735ac164420297
Juliana Villegas <jumavipe@hotmail.com>
2022-08-09 07:46:14 -0500

    added code styles

--- src/components/DisplayConcept.vue
+++ src/components/DisplayConcept.vue
@@ -2,26 +2,46 @@
 export default {
   data() {
     return {
-      isSelected : true
-    }
-  }
-}
+      isSelected: true,
+    };
+  },
+};
 </script>
 
 <template>
- <section>
-  <h2>Data, Methods, Computed</h2>
-  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam dolorum itaque aperiam quos harum enim veritatis, possimus ipsa distinctio repellat, tenetur esse earum voluptatibus consectetur repellendus nam non consequuntur pariatur!</p>
+  <section>
+    <h2>Data, Methods, Computed</h2>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam dolorum
+      itaque aperiam quos harum enim veritatis, possimus ipsa distinctio
+      repellat, tenetur esse earum voluptatibus consectetur repellendus nam non
+      consequuntur pariatur!
+    </p>
+    <pre>
   <code>{
-    data: isSelected}</code>
- </section>
+    data() {
+      return {
+        isSelected: true
+        }
+    }</code>
+    </pre>
+  </section>
 </template>
 
 <style scoped>
 section {
   margin: 20px;
   padding: 20px;
-  background-color: #EEE;
+  background-color: #eee;
   width: 1000px;
+  height: fit-content;
+}
+p {
+  margin-bottom: 16px;
+}
+pre {
+  border-radius: 5px;
+  padding: 8px;
+  background-color: #dfdfdf;
 }
 </style>
